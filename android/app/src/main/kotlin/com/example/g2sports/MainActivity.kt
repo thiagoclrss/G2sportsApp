2bcc6fb017e23d40cada559f7c215fb7531bc603
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-11-04 19:18:27 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/g2sports/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.g2sports
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    child: Text("G2 Sports"),
+  ));
+}
