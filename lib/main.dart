95ceb85028467b2485704d424e8cb738f8c6e072
Thiago Colares <thiagocolaress77@gmail.com>
2021-12-09 01:25:07 -0300

    desenvolvimento da homeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:g2sports/ui/home_page.dart';
 
+//import 'package:buscador_gifs/ui/gif_page.dart';
 void main() {
-  runApp(Container(
-    child: Text("G2 Sports"),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
 }
